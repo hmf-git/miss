d4d9ca2e20305d4529a24bb07c26bce7aa4d617e
877506743@qq.com <877506743@qq.com>
2020-12-04 12:21:53 +0800

    hemingfeng

---
+++ student/src/student/miss.java
@@ -0,0 +1,16 @@
+package student;
+
+public class miss {
+
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
